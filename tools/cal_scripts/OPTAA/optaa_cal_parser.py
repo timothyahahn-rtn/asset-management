bde45612d821cc095999a6e0e9875a28794bcf90
Daniel Tran <96daniel.tran@gmail.com>
2018-03-22 17:44:20 -0700

    Fixed error with OPTAA parser. Forgot to include path to include python that has shared code

--- tools/cal_scripts/OPTAA/optaa_cal_parser.py
+++ tools/cal_scripts/OPTAA/optaa_cal_parser.py
@@ -9,6 +9,7 @@ import json
 import sys
 import string
 import time
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from cal_parser_template import Calibration, get_uid_serial_mapping
 
 class OPTAACalibration(Calibration):
