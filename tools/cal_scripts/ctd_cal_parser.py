4b9036a331a79c8b02b050b38025d444ea2a2f9a
funnyabc <96daniel.tran@gmail.com>
2018-10-08 09:49:42 -0700

    Changed code to have common fuction to put used calibration files to archive. Added back missing manufacture directory

--- tools/cal_scripts/ctd_cal_parser.py
+++ tools/cal_scripts/ctd_cal_parser.py
@@ -163,8 +163,7 @@ def main():
                     cal.read_cal(os.path.join(path, file))
                 cal.asset_tracking_number = lookup[cal.serial]
                 cal.write_cal_info()
-                shutil.move(os.path.join(os.getcwd(), 'CTD', 'manufacturer', file), \
-                        os.path.join(os.getcwd(), 'CTD', 'manufacturer_ARCHIVE', file))
+                cal.move_to_archive(cal.type, file)
 
 if __name__ == '__main__':
     start_time = time.time()
