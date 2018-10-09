bba14e3c994987642fa04d51e2f6f5f625b008af
funnyabc <96daniel.tran@gmail.com>
2018-10-09 15:30:17 -0700

    Added missing DOFSTA file

--- tools/cal_scripts/dofsta_cal_parser.py
+++ tools/cal_scripts/dofsta_cal_parser.py
@@ -9,20 +9,44 @@ import datetime
 import os
 import sys
 import time
+import xml.etree.ElementTree as et
 from common_code.cal_parser_template import Calibration, get_uid_serial_mapping
 
 class SBE43Calibration(Calibration):
     def __init__(self):
         super(SBE43Calibration, self).__init__()
         self.type = 'DOFSTA'
+        self.serial = '43-'
         self.coefficient_name_map = {
             'E': 'CC_residual_temperature_correction_factor_e',
             'C': 'CC_residual_temperature_correction_factor_c',
             'VOFFSET': 'CC_voltage_offset',  # note that this was previously called CC_frequency_offset
+            'OFFSET': 'CC_voltage_offset',
             'SOC': 'CC_oxygen_signal_slope',
             'A': 'CC_residual_temperature_correction_factor_a',
             'B': 'CC_residual_temperature_correction_factor_b',
         }
+
+    def read_xml(self, filename):
+        with open(filename) as fh:
+            tree = et.parse(filename)
+            root = tree.getroot()
+            t_flag = False
+            for child in tree.iter():
+                key = child.tag.upper()
+                print(child.tag)
+                if key == '':
+                    continue
+
+                if child.tag == "SerialNumber" and child.text != None and self.serial == '43-':
+                    self.serial = '43-' + child.text
+
+                if child.tag == "CalibrationDate" and child.text != None and self.date == None:
+                    self.date = datetime.datetime.strptime(child.text, "%d-%b-%y").strftime("%Y%m%d")
+                name = self.coefficient_name_map.get(key)
+                if name is None:
+                    continue
+                self.coefficients[name] = child.text
 
     def read_cal(self, filename):
         with open(filename) as fh:
@@ -36,7 +60,7 @@ class SBE43Calibration(Calibration):
                 value = parts[1].strip()
 
                 if key == 'INSTRUMENT_TYPE' and value != 'SBE43':
-                    print 'Error - unexpected type calibration file (%s != SBE43)' % value
+                    print('Error - unexpected type calibration file (%s != SBE43)' % value)
                     sys.exit(1)
 
                 if self.coefficient_name_map.has_key(key):
@@ -64,10 +88,15 @@ def main():
             if file[0] == '.':
                 continue
             cal = SBE43Calibration()
-            cal.read_cal(os.path.join(path, file))
-            cal.asset_tracking_number = lookup[cal.serial]
-            cal.write_cal_info()
-            cal.move_to_archive(cal.type, file)
+            with open(os.path.join(path, file)) as unknown_file:
+                c = unknown_file.read(1)
+                if c == '<':
+                    cal.read_xml(os.path.join(path, file))
+                else:
+                    cal.read_cal(os.path.join(path, file))
+                cal.asset_tracking_number = lookup[cal.serial]
+                cal.write_cal_info()
+                cal.move_to_archive(cal.type, file)
 
 
 if __name__ == '__main__':
